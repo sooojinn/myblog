1c6c5b7f5b38464a6b8f1148282328eedd296dea
soojinpark99 <tnwls7263@gmail.com>
2024-05-14 17:06:16 +0900

    반응형 css 추가

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -2,25 +2,35 @@ import Link from "next/link";
 import { getCategoryList } from "/lib/posts";
 import styles from "@/styles/CategoryList.module.css";
 import { CountPosts } from "/lib/posts";
-import { changeCategoryName } from "../../lib/posts";
+import { changeCategoryName } from "/lib/posts";
 
 export default function CategoryList({ currentCategory }) {
   const categoryList = getCategoryList();
   categoryList.unshift("");
 
+  const renderCategory = (category) => {
+    return (
+      <>
+        {changeCategoryName(category || "all")} ({CountPosts(category)})
+      </>
+    );
+  };
+
   return (
-    <nav className={styles.nav}>
-      {categoryList.map((category) => {
-        return (
-          <Link
-            href={`/posts/${category}`}
-            className={currentCategory == category ? styles.active : ""}
-            key={category}
-          >
-            {changeCategoryName(category || "all")} ({CountPosts(category)})
-          </Link>
-        );
-      })}
-    </nav>
+    <>
+      <nav className={styles.nav}>
+        {categoryList.map((category) => {
+          return (
+            <Link
+              href={`/posts/${category}`}
+              className={currentCategory == category ? styles.active : ""}
+              key={category}
+            >
+              {renderCategory(category)}
+            </Link>
+          );
+        })}
+      </nav>
+    </>
   );
 }
