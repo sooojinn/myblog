5b3f1622c203b6012e31384ebeba6b5a060e12b8
soojinpark99 <tnwls7263@gmail.com>
2024-05-20 15:57:54 +0900

    글 상세 페이지 css 설정

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -39,7 +39,7 @@ export default function PostList({ postList }) {
       <ul className={styles.postList}>
         {postListInPage.map((post) => (
           <Link href={post.url} className={styles.post} key={post.slug}>
-            <h2>{post.title}</h2>
+            <h3>{post.title}</h3>
             <p className={styles.desc}>{post.desc}</p>
             <p className={styles.date}>{post.date}</p>
           </Link>
