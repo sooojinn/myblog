ff3c93a9f7aab13c0bc63a56e45d146795865902
soojinpark99 <tnwls7263@gmail.com>
2024-05-21 19:58:23 +0900

    vercel.json 생성

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,9 @@ export default function Header() {
           <h1>{BLOG_NAME}</h1>
         </Link>
         <div className={styles.icons}>
-          <Link href="https://github.com/sooojinn">GitHub</Link>
+          <Link href="https://github.com/sooojinn">
+            <Image src="/gitHub_icon.png" alt="GitHub" width={30} height={30} />
+          </Link>
         </div>
         <nav className={styles.nav}>
           <Link
