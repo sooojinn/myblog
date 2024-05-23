710205fab9c002fc88aaa9be54592437461639ee
soojinpark99 <tnwls7263@gmail.com>
2024-05-23 13:55:07 +0900

    Feat: light/dark 모드 토글 버튼 구현

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,29 +2,27 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi2";
+import styles from "@/styles/ThemeToggle.module.css";
 
 export default function ThemeToggle() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  useEffect(() => {
-    console.log("Theme changed to:", theme);
-  }, [theme]);
-
   if (!mounted) return null;
 
-  return (
-    <button
-      onClick={() => {
-        setTheme(theme === "dark" ? "light" : "dark");
-        console.log(theme);
-      }}
-    >
-      Toggle Theme
+  return mounted && currentTheme === "dark" ? (
+    <button className={styles.toggleBtn} onClick={() => setTheme("light")}>
+      <HiOutlineSun size={20} />
+    </button>
+  ) : (
+    <button className={styles.toggleBtn} onClick={() => setTheme("dark")}>
+      <HiOutlineMoon size={20} />
     </button>
   );
 }
