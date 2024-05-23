22e1b057dba2d61c9e1e80789ea7443a3d9fa69e
soojinpark99 <tnwls7263@gmail.com>
2024-05-23 12:25:08 +0900

    Design: Aside 구분선 끝까지 닿게 수정

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,12 +1,13 @@
 import CategoryList from "./CategoryList";
 import { getRenderedCategoryList } from "/lib/posts";
+import styles from "@/styles/Aside.module.css";
 
 export default function Aside() {
   const renderedCategoryList = getRenderedCategoryList();
 
   return (
-    <>
+    <aside className={styles.aside}>
       <CategoryList renderedCategoryList={renderedCategoryList} />
-    </>
+    </aside>
   );
 }
