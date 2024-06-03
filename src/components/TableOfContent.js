9cd98509a480632b76ff0b903ebf9b09ce406ffd
soojinpark99 <tnwls7263@gmail.com>
2024-06-03 10:40:12 +0900

    Feat: Toc 현재 목차 하나만 표시되게 수정

--- src/components/TableOfContent.js
+++ src/components/TableOfContent.js
@@ -8,27 +8,37 @@ import { useEffect, useState } from "react";
 export default function TableOfContent({ data }) {
   const slugger = new GithubSlugger();
   const headings = data.split("\n").filter((str) => str.match(/^#+/));
-  const [currentHeading, setCurrentHeading] = useState([]);
+  const [currentHeading, setCurrentHeading] = useState();
 
   useEffect(() => {
     const observerOptions = {
       root: null, // 기본값, 뷰포트 기준
       rootMargin: "-20px 0px", // root의 범위를 확장하거나 축소할 수 있다
-      threshold: 0, // 타겟이 40% 이상 보여질 때 실행
+      threshold: 1, // 타겟이 40% 이상 보여질 때 실행
     };
 
-    // 타겟이 최초 등록될 시 그리고 가시성에 변화가 생길 시 실행되는 콜백
+    // 마지막으로 교차한 요소의 ID를 저장하는 변수를 초기화합니다.
+    let lastIntersectingId = null;
+
     const observerCallback = (entries) => {
+      // 교차 상태가 변했는지 여부를 추적하는 변수를 초기화합니다.
+      let anyIntersecting = false;
+
       entries.forEach((entry) => {
         const targetId = entry.target.id;
+
         if (entry.isIntersecting) {
-          setCurrentHeading((prev) => [...prev, targetId]);
-        } else {
-          setCurrentHeading((prev) => {
-            return prev.filter((elem) => elem !== targetId);
-          });
+          // 요소가 교차하고 있을 때의 처리
+          lastIntersectingId = targetId; // 마지막으로 교차한 요소의 ID를 업데이트
+          anyIntersecting = true; // 교차하는 요소가 있음을 표시
+          setCurrentHeading(targetId);
         }
       });
+
+      // 교차하는 요소가 없을 때 lastIntersectingId를 currentHeading으로 설정
+      if (!anyIntersecting && lastIntersectingId) {
+        setCurrentHeading(lastIntersectingId);
+      }
     };
 
     // new IntersectionObserver()를 통해 생성한 인스턴스로 관측자를 초기화
@@ -60,7 +70,7 @@ export default function TableOfContent({ data }) {
             <li
               key={headingLink}
               className={`${styles[`toc-level-${level}`]} ${
-                currentHeading.includes(headingLink) ? styles.current : ""
+                currentHeading === headingLink ? styles.current : ""
               } ${styles.heading}`}
             >
               <Link href={`#${headingLink}`}>{headingText}</Link>
