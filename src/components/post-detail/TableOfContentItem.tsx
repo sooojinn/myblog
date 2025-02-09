f8be1cbfcbbb1015ff106cab7f2db6071bc25522
soojinpark99 <tnwls7263@gmail.com>
2025-02-09 18:29:52 +0900

    doc: react-virtual 포스트 내용 추가

--- src/components/post-detail/TableOfContentItem.tsx
+++ src/components/post-detail/TableOfContentItem.tsx
@@ -9,9 +9,15 @@ export default function TableOfContentItem({
   isActive,
   children,
 }: TableOfContentItemProps) {
+  const paddingLeft: Record<number, string> = {
+    0: "pl-[1em]",
+    1: "pl-[2em]",
+    2: "pl-[3em]",
+  };
+
   return (
     <div
-      className={`${depth === 1 ? "pl-[2em]" : "pl-[1em]"} ${
+      className={`${paddingLeft[depth]} ${
         isActive
           ? "text-main underline lg:no-underline lg:bg-gray-100 dark:lg:bg-gray-700 lg:text-gray-700 dark:lg:text-gray-100 lg:border-l-[3px] lg:border-main"
           : "text-gray-400 hover:text-main lg:border-l-[3px] lg:border-gray-100 dark:lg:border-gray-800"
